986376f4a958c03a83b3ff3f253f838a38b1e1a8
Thomas Cheng <thomas.cheng@loopio.com>
2020-04-25 15:20:12 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const app = require('express')();
+const express = require('express');
 const path = require('path');
-const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build'));
 });
 
@@ -18,6 +21,7 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 });
-http.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
