9594ff1bcbe41282bc58a390f82953ba165a7741
Thomas Cheng <thomas.cheng@loopio.com>
2020-04-25 14:10:20 -0400

    Update port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
     res.send("Hello world");
     // res.sendFile(__dirname + '/index.html');
@@ -16,6 +18,6 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 });
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
