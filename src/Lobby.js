01fc4684c3170453fc8ebef74bb4af8412f4b7be
Thomas Cheng <thomas.cheng@loopio.com>
2020-04-26 10:24:11 -0400

    Update lobby layout

--- src/Lobby.js
+++ src/Lobby.js
@@ -2,108 +2,54 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Lobby = ({ initialName, invalidCode, onCreateRoom, onJoinRoom }) => {
-  const [mode, setMode] = useState(null); // create or join
   const [name, setName] = useState(initialName);
   const [code, setCode] = useState("");
 
-  if (!mode) {
-    return (
+  return (
+    <div>
       <div>
-        <button
-          onClick={() => {
-            setMode("create");
-          }}
-        >
-          Create
-        </button>
-        <button
-          onClick={() => {
-            setMode("join");
-          }}
-        >
-          Join
-        </button>
+        <label htmlFor="name">Name:</label>
       </div>
-    );
-  }
-
-  if (mode === "create") {
-    return (
       <div>
-        <form
-          onSubmit={(evt) => {
-            evt.preventDefault();
-            onCreateRoom({ name });
+        <input
+          id="name"
+          name="name"
+          value={name}
+          onChange={(evt) => {
+            setName(evt.target.value);
           }}
-        >
-          <label htmlFor="name">Name:</label>
+        />
+      </div>
+      <div>
+        <div>
           <input
-            id="name"
-            name="name"
-            value={name}
+            placeholder="CODE"
+            id="code"
+            name="code"
+            value={code}
             onChange={(evt) => {
-              setName(evt.target.value);
+              setCode(evt.target.value);
             }}
           />
-          <button type="submit">Create Game</button>
-        </form>
-        <button
-          onClick={() => {
-            setMode(null);
-          }}
-        >
-          Back
-        </button>
+          <button
+            onClick={() => {
+              onJoinRoom({ code, name });
+            }}
+          >
+            Join
+          </button>
+          <button
+            onClick={() => {
+              onCreateRoom({ name });
+            }}
+          >
+            Start New Game
+          </button>
+        </div>
+        {invalidCode && <div>Code not found</div>}
       </div>
-    );
-  }
-
-  if (mode === "join") {
-    return (
-      <div>
-        <form
-          onSubmit={(evt) => {
-            evt.preventDefault();
-            onJoinRoom({ code, name });
-          }}
-        >
-          <div>
-            <label htmlFor="code">Game Code:</label>
-            <input
-              id="code"
-              name="code"
-              value={code}
-              onChange={(evt) => {
-                setCode(evt.target.value);
-              }}
-            />
-            {invalidCode && <span>Code not found</span>}
-          </div>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              id="name"
-              name="name"
-              value={name}
-              onChange={(evt) => {
-                setName(evt.target.value);
-              }}
-            />
-          </div>
-          <button type="submit">Join Game</button>
-        </form>
-        <button
-          onClick={() => {
-            setMode(null);
-          }}
-        >
-          Back
-        </button>
-      </div>
-    );
-  }
-
-  return null;
+    </div>
+  );
 };
 
 Lobby.propTypes = {
